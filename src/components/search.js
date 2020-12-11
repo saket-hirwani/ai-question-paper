6560a8548a9ada45d1d8126d16ccdeabafc4c61b
saket-hirwani <sakethirwani15@gmail.com>
2020-12-11 17:50:25 +0530

    Search UI

--- src/components/search.js
+++ src/components/search.js
@@ -49,8 +49,14 @@ function Search() {
   const [branch, setbranch] = useState([]);
   const [semester, setsemester] = useState([]);
   const [subject, setsubject] = useState([]);
+  const [year, setyear] = useState([])
 
-  // const [fname, setfname] = useState("saket")
+
+  const [universityname, setuniversityname] = useState(null);
+  const [branchname, setbranchname] = useState(null);
+  const [semname, setsemname] = useState(null);
+  const [subname, setsubname] = useState(null);
+  // const [yearname, setyearname] = useState(null);
   function customTheme(theme) {
     return {
       ...theme,
@@ -62,8 +68,7 @@ function Search() {
     };
   }
   const handleUniversityChange = (event) => {
-    var data = event.target.value;
-    setuniversity(data);
+    setuniversityname(event.target.value)
     var val = datastore.filter((item) => {
       return item.data.name == event.target.value;
     });
@@ -82,6 +87,7 @@ function Search() {
     console.log("val", event.target.value);
   };
   const handleBranchChange = (event) => {
+    setbranchname(event.target.value)
     var val = branch.filter((item) => {
       return item.data.bname == event.target.value;
     });
@@ -101,6 +107,7 @@ function Search() {
       );
   };
   const handleSemChange = (event) => {
+    setsemname(event.target.value)
     var val = semester.filter((item) => {
       return item.data.sem == event.target.value;
     });
@@ -122,23 +129,30 @@ function Search() {
       );
   };
   const handlesubjectChange = (event) => {
+    setsubname(event.target.value)
     var val = subject.filter((item) => {
       return item.data.sname == event.target.value;
     });
     setsubId(val[0].id);
-    // db.collection('university').doc(universityId).collection('branch').doc(branchId).collection('semester').doc(val[0].id).collection('subject').onSnapshot(snapshot=>(
-    //   setsubject(snapshot.docs.map(doc=>(
-    //         {
-    //             id: doc.id,
-    //             data: doc.data(),
-    //           }
-    //           )))
-    //           ))
+    db.collection("university")
+      .doc(universityId)
+      .collection("branch")
+      .doc(branchId)
+      .collection("semester")
+      .doc(semId)
+      .collection("subject")
+      .doc(val[0].id)
+      .collection("year")
+      .onSnapshot((snapshot) =>
+        setyear(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        ),
+        
+      );
   };
-
-  // const InsideBranch = Branch[university];
-  // const InsideSemester = Semester[university];
-  // const InsideSubject = Subject;
   return (
     <Container
       maxWidth="md"
@@ -147,7 +161,9 @@ function Search() {
       <h1 style={{ textAlign: "center" }}>Select your university details</h1>
       <FormControl className={classes.formControl}>
         <h5>University</h5>
+        <div className="upload_select">
         <select onChange={handleUniversityChange}>
+        <option>--select--</option>
           {datastore.map((item, index) => {
             return (
               <option key={index} value={item.data.id}>
@@ -156,10 +172,13 @@ function Search() {
             );
           })}
         </select>
+        </div>
       </FormControl>
       <FormControl className={classes.formControl}>
         <h5>Branch</h5>
+        <div className="upload_select">
         <select onChange={handleBranchChange}>
+        <option>--select--</option>
           {branch.map((item, index) => {
             return (
               <option key={index} value={item.data.id}>
@@ -168,10 +187,13 @@ function Search() {
             );
           })}
         </select>
+        </div>
       </FormControl>
       <FormControl className={classes.formControl}>
         <h5>Semester</h5>
+        <div className="upload_select">
         <select onChange={handleSemChange}>
+        <option>--select--</option>
           {semester.map((item, index) => {
             return (
               <option key={index} value={item.data.id}>
@@ -180,10 +202,13 @@ function Search() {
             );
           })}
         </select>
+        </div>
       </FormControl>
       <FormControl className={classes.formControl}>
         <h5>Subject</h5>
+        <div className="upload_select">
         <select onChange={handlesubjectChange}>
+        <option>--select--</option>
           {subject.map((item, index) => {
             return (
               <option key={index} value={item.data.id}>
@@ -192,6 +217,7 @@ function Search() {
             );
           })}
         </select>
+        </div>
       </FormControl>
       {/*<FormControl className={classes.formControl}>
       <h5>Year</h5>
@@ -208,7 +234,7 @@ theme={customTheme}
         <Link
           to={{
             pathname: "/qp",
-            state: { university, branch, semester, subject },
+            state: { universityname, branchname, subname, semname, year, universityId, branchId, semId, subId },
           }}
         >
           <Button
